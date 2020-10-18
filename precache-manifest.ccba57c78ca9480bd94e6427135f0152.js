191b649c32451cdae3c2fa9dff21839da159cebd
Menachem Neiman <Nerevar123@gmail.com>
2020-10-18 17:14:31 +0300

    Updates

---
+++ precache-manifest.ccba57c78ca9480bd94e6427135f0152.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efce709c561be46221a380ecff11e04",
+    "url": "/architect-portfolio-react/index.html"
+  },
+  {
+    "revision": "4b18c9d276322dea47ca",
+    "url": "/architect-portfolio-react/static/css/main.953edc1f.chunk.css"
+  },
+  {
+    "revision": "94a3511bc18b66c17200",
+    "url": "/architect-portfolio-react/static/js/2.515755f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/architect-portfolio-react/static/js/2.515755f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b18c9d276322dea47ca",
+    "url": "/architect-portfolio-react/static/js/main.f828bab4.chunk.js"
+  },
+  {
+    "revision": "bd38d361c1844feeb068",
+    "url": "/architect-portfolio-react/static/js/runtime-main.8544f5ab.js"
+  },
+  {
+    "revision": "abccbe06d69b9a136cf43e67d7a55412",
+    "url": "/architect-portfolio-react/static/media/1.1.abccbe06.png"
+  },
+  {
+    "revision": "0d58c4201ada250beb6507e800017f93",
+    "url": "/architect-portfolio-react/static/media/1.2.0d58c420.png"
+  },
+  {
+    "revision": "1cd36aec4803333aaa848e5099ee03ac",
+    "url": "/architect-portfolio-react/static/media/1.3.1cd36aec.png"
+  },
+  {
+    "revision": "fac2c84b049820e5d6afe897bd6589d3",
+    "url": "/architect-portfolio-react/static/media/1.4.fac2c84b.png"
+  },
+  {
+    "revision": "04f85c96a5addf4abffca62adb12af9e",
+    "url": "/architect-portfolio-react/static/media/1.5.04f85c96.png"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/architect-portfolio-react/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/architect-portfolio-react/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/architect-portfolio-react/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "23d74a4599f3c1358cba0dc69949373f",
+    "url": "/architect-portfolio-react/static/media/bio-portrait.23d74a45.jpg"
+  },
+  {
+    "revision": "e634f9fe72709e6ebf42b3e18349e5a1",
+    "url": "/architect-portfolio-react/static/media/bio-scroll.e634f9fe.svg"
+  },
+  {
+    "revision": "e7d5d2b167676f024bb7487dd428c93c",
+    "url": "/architect-portfolio-react/static/media/icon 1.e7d5d2b1.svg"
+  },
+  {
+    "revision": "38bac4e94f0ee5e457fa2a3c03909680",
+    "url": "/architect-portfolio-react/static/media/icon 2.38bac4e9.svg"
+  },
+  {
+    "revision": "d36a63322b830e5ce7b3fb01082b1f5b",
+    "url": "/architect-portfolio-react/static/media/icon 3.d36a6332.svg"
+  },
+  {
+    "revision": "817ba8654f9554b9ab7e2b283539025d",
+    "url": "/architect-portfolio-react/static/media/work(1).817ba865.png"
+  },
+  {
+    "revision": "77ff2650be094608e785a443a732073e",
+    "url": "/architect-portfolio-react/static/media/work(2).77ff2650.png"
+  },
+  {
+    "revision": "cdcdcdef23dfc35716bedac4f2c2691a",
+    "url": "/architect-portfolio-react/static/media/work(3).cdcdcdef.png"
+  },
+  {
+    "revision": "3066812c91e427a382865463ac875ff2",
+    "url": "/architect-portfolio-react/static/media/work(4).3066812c.png"
+  },
+  {
+    "revision": "82fa0f8003289cb5e3dd189f7518bda3",
+    "url": "/architect-portfolio-react/static/media/work(5).82fa0f80.png"
+  },
+  {
+    "revision": "f177640c0b0c21aa2808dcfbdc585307",
+    "url": "/architect-portfolio-react/static/media/work(6).f177640c.png"
+  },
+  {
+    "revision": "b07c7b02c9d8bde88a6026bc45008e09",
+    "url": "/architect-portfolio-react/static/media/work(7).b07c7b02.png"
+  }
+]);
